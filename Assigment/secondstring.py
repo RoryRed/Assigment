9bc792b50cad1b0f81c694c52bff1beb72cc2566
RoryRed <60267332+RoryRed@users.noreply.github.com>
2020-04-03 17:18:21 +0100

    Delete secondstring.py

--- Assigment/secondstring.py
+++
@@ -1,7 +0,0 @@
-x = input("Please enter a sentence:") #input string
-reversed ="".join(reversed(x)) #reverses sentence 
-
-print(reversed[0:10000:2])#outputs every second letter in revese order
-
-
-#https://www.w3schools.com/python/python_howto_reverse_string.asp
