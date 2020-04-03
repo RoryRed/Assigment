2e4e292bd2925f1e923cbe78e6ec5cf54d79f4a6
RoryRed <60267332+RoryRed@users.noreply.github.com>
2020-04-03 17:19:34 +0100

    Delete bmi.py

--- Assigment/bmi.py
+++
@@ -1,8 +0,0 @@
-#Rory Redmond
-#formula for caluating bmi based off user inputs 
-
-
-height= float(input("Enter Height in cm : "))/100 #formula to convert height in cm to m
-Weight= float(input("Enter weight in kg :"))#input of weigt in kg
-Bmi = Weight/(height**2)#uses vaules for height and width to calculate BMI
-print(Bmi)
